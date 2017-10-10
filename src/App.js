6946838e12361df862f76547f858e9d6eb3fd5d7
aibin <1149299088@qq.com>
2017-10-10 14:15:07 +0800

    初始化leancloud

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import 'normalize.css';
+import './reset.css';
+import './App.css';
+import TodoInput from './TodoInput';
+import TodoItem from './TodoItem';
+import * as localStore from './localStore';
+import AV from 'leancloud-storage';
+
+var APP_ID = 'sUazgAf7YdKpcVpMF2kbikST-gzGzoHsz';
+var APP_KEY = '3AFclxyoKCfMjxceoadSzyJF';
+
+AV.init({
+	appId: APP_ID,
+	appKey: APP_KEY
+});
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: '',
+      todoList: localStore.load('todoList') || []
+    };
+  }
+
+  render() {
+    let todos = this.state.todoList
+      .filter((item)=> !item.deleted)
+      .map((item, index)=> {
+	    return (
+        <li key={index}>
+          <TodoItem todo={item}
+                    onToggle={this.toggle.bind(this)}
+                    onDelete={this.delete.bind(this)}/>
+        </li>
+	    )
+    });
+    return (
+      <div className="App">
+        <h1>My To Do</h1>
+        <div className="inputWrapper">
+          <TodoInput content={this.state.newTodo}
+                     onChange={this.changeTitle.bind(this)}
+                     onSubmit={this.addTodo.bind(this)}/>
+        </div>
+        <ol className="todoList">
+          {todos}
+        </ol>
+      </div>
+    );
+  }
+
+  componentDidUpdate() {
+	  localStore.save('todoList', this.state.todoList);
+  }
+
+  toggle(e, todo) {
+    todo.status = todo.status === 'completed' ? '' : 'completed';
+    this.setState(this.state);
+  }
+  changeTitle(event) {
+    this.setState({
+      newTodo: event.target.value,
+      todoList: this.state.todoList
+    });
+  }
+  addTodo(event) {
+    this.state.todoList.push({
+      id: idMaker(),
+      title: event.target.value,
+      status: null,
+      deleted: false
+    });
+    this.setState({
+      newTodo: '',
+      todoList: this.state.todoList
+    });
+  }
+  delete(event, todo) {
+    todo.deleted = true;
+    this.setState(this.state);
+  }
+}
+
+export default App;
+
+let id = 0;
+function idMaker() {
+  id += 1;
+  return id;
+}
---
+++ src/TodoInput.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './TodoInput.css';
+
+export default class TodoInput extends Component {
+	render() {
+		return <input type="text" className="TodoInput"
+		              value={this.props.content}
+		              onChange={this.changeTitle.bind(this)}
+		              onKeyPress={this.submit.bind(this)}/>
+	}
+	submit(e) {
+		if (e.key === 'Enter') {
+			this.props.onSubmit(e);
+		}
+	}
+	changeTitle(e) {
+		this.props.onChange(e);
+	}
+}
