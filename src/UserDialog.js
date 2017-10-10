60b6bee78a8b9a9b91b0d1f2fe56651a72689183
aibin <1149299088@qq.com>
2017-10-10 22:05:36 +0800

    完成登录功能

--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './UserDialog.css';
-import {signUp} from './leanCloud'
+import {signUp, signIn} from './leanCloud'
 
 export default class UserDialog extends Component {
 	constructor(props) {
@@ -25,20 +25,46 @@ export default class UserDialog extends Component {
 			this.props.onSignUp.call(null, user);
 		}
 		let error = (error)=> {
-			console.log(error);
+			switch(error.code) {
+				case 202:
+					alert('用户名已被占用');
+					break
+				default:
+					alert(error);
+					break
+			}
 		}
 		signUp(username, password, success, error);
 	}
 	signIn(e) {
-
+		e.preventDefault();
+		let {username, password} = this.state.formData;
+		let success = (user)=> {
+			this.props.onSignIn.call(null, user);
+		}
+		let error = (error)=> {
+			switch(error.code) {
+				case 210:
+					alert('用户名和密码不匹配');
+					break
+				case 201:
+					alert('密码为空');
+					break
+				case 211:
+					alert('找不到用户');
+					break
+				default:
+					alert(error);
+					break
+			}
+		};
+		signIn(username, password, success, error);
 	}
 	changeFormData(key, e) {
 		let stateCopy = JSON.parse(JSON.stringify(this.state));
 		stateCopy.formData[key] = e.target.value;
 		this.setState(stateCopy);
 	}
-
-
 
 	render() {
 		let signUpForm = (
